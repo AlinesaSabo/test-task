45fc7f5791565f1a07e68d91146d0dcb22dd4874
Alinesa <alinesa322@gmail.com>
2025-03-09 23:53:57 +0200

    fix

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,33 +1,7 @@
 import React from "react";
 import CardTeam from "../CardTeam/CardTeam";
 import s from "./Team.module.css";
-
-const teamMembers = [
-  {
-    id: 1,
-    title: "Member of our expert team",
-    image:
-      "/src/assets/image/team/businessmen-internet-technology-commerce-bitcoin-mining-studio-official-removebg-preview.png",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo ipsum aliquet turpis elit elit natoque varius eget facilisi. Amet rhoncus",
-  },
-  {
-    id: 2,
-    title: "Member of our expert team",
-    image:
-      "/src/assets/image/team/young-african-american-girl-teen-student-denim-clothes-backpack-hold-bitcoin-isolated-blue-wall-min-removebg-preview.png",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo ipsum aliquet turpis elit elit natoque varius eget facilisi. Amet rhoncus",
-  },
-  {
-    id: 3,
-    title: "Member of our expert team",
-    image:
-      "/src/assets/image/team/portrait-confident-businessman-dressed-suit-removebg-preview.png",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo ipsum aliquet turpis elit elit natoque varius eget facilisi. Amet rhoncus",
-  },
-];
+import teamMembers from "../../data/team.json";
 
 const Team = () => {
   return (
