2d2dcaca1045b48b8fa54ac824543faeec1f85ee
Alinesa <alinesa322@gmail.com>
2025-03-10 19:06:43 +0200

    add section module

--- src/components/ModuleCard/ModuleCard.jsx
+++ src/components/ModuleCard/ModuleCard.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import s from "./ModuleCard.module.css";
+import Img from "../../assets/image/fon/Rectangle 39227.png";
 
-const ModuleCard = ({ title, description, skills }) => {
+const ModuleCard = ({ title, description, skills, img }) => {
   return (
     <div className={s.card}>
-      <h2 className={s.title}>{title}</h2>
-      <p className={s.description}>{description}</p>
-      <div className={s.skills}>
-        {skills.map((skill, index) => (
-          <span key={index} className={s.skill}>
-            {skill}
-          </span>
-        ))}
+      <div className={s.cardTitle}>
+        <h2 className={s.title}>{title}</h2>
+        <p className={s.description}>{description}</p>
+        <p className={s.descriptionSkills}>What we will learn and do:</p>
+        <div className={s.skills}>
+          {skills.map((skill, index) => (
+            <span key={index} className={s.skill}>
+              {skill}
+            </span>
+          ))}
+        </div>
+        <img src={img} alt="image" className={s.image} />
       </div>
+      <img src={Img} alt="Img" className={s.img} />
     </div>
   );
 };
--- src/components/SectionModule/SectionModule.jsx
+++ src/components/SectionModule/SectionModule.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import s from "./SectionModule.module.css";
 import ModuleCard from "../ModuleCard/ModuleCard";
+import safe from "../../assets/image/safe/image 501.png";
+import safe2 from "../../assets/image/safe/image 501 (1).png";
+import safe3 from "../../assets/image/safe/image 501 (2).png";
+import bg from "..//../assets/Group 3.png";
 
 const modules = [
   {
     title: "Module 1: The foundation",
+    image: safe,
     description:
       "Discover the most promising niche in cryptocurrencies where you can make decent money on your own",
     skills: ["Apps Development", "SEO Services", "Data Analysis"],
   },
   {
     title: "Module 2: The foundation",
+    image: safe2,
     description:
       "Discover the most promising niche in cryptocurrencies where you can make decent money on your own",
     skills: ["Apps Development", "SEO Services", "Data Analysis"],
   },
   {
     title: "Module 3: The foundation",
+    image: safe3,
     description:
       "Discover the most promising niche in cryptocurrencies where you can make decent money on your own",
     skills: ["Apps Development", "SEO Services", "Data Analysis"],
@@ -26,24 +33,28 @@ const modules = [
 const SectionModule = () => {
   return (
     <section className={s.section}>
-      <div className={s.description}>
-        <h5>Lorem ipsum</h5>
-        <h2>Amet pretium</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo ipsum
-          aliquet turpis elit elit natoque varius eget facilisi. Amet rhoncus
-        </p>
+      <div className={s.container}>
+        <div className={s.description}>
+          <h5>Lorem ipsum</h5>
+          <h2>Amet pretium</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo ipsum
+            aliquet turpis elit elit natoque varius eget facilisi. Amet rhoncus
+          </p>
+        </div>
+        <div className={s.list}>
+          {modules.map((module, index) => (
+            <ModuleCard
+              key={index}
+              title={module.title}
+              description={module.description}
+              skills={module.skills}
+              img={module.image}
+            />
+          ))}
+        </div>
       </div>
-      <div className={s.list}>
-        {modules.map((module, index) => (
-          <ModuleCard
-            key={index}
-            title={module.title}
-            description={module.description}
-            skills={module.skills}
-          />
-        ))}
-      </div>
+      <img src={bg} alt="bg" className={s.bg} />
     </section>
   );
 };
